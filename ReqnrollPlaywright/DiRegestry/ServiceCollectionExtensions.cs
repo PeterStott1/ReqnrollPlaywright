2a39bdc236e246a1ba6686a82b4a4662f4d182ca
peter stott <peter.stott@vitality.co.uk>
2026-09-14 16:34:26 +0100

    added comments

--- ReqnrollPlaywright/DiRegestry/ServiceCollectionExtensions.cs
+++ ReqnrollPlaywright/DiRegestry/ServiceCollectionExtensions.cs
@@ -1,12 +1,21 @@
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ReqnrollPlaywright.Models;
 
 namespace ReqnrollPlaywright.DiRegestry
 {
+    /// <summary>
+    /// extension static class used to collect
+    /// the configuration to use per environment
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Creates and configures a service provider for Playwright tests by
+        /// loading environment-specific settings from the Support folder.
+        /// Defaults to the Development configuration when TEST_ENV is not set.
+        /// </summary>
+        /// <returns>built service provider</returns>
         public static IServiceProvider AddPlaywrightConfiguration()
         {
             var services = new ServiceCollection();
